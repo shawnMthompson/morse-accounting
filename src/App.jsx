a10cffc063e7e31951d16fa57b64f039ecac0adf
Shawn Thompson <137923627+shawnMthompson@users.noreply.github.com>
2026-01-26 20:40:44 -0500

    Added primary color theme variables and updated heading color for testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 export default function App() {
   return(
-    <h1 className="text-4xl font-bold text-blue-500 flex justify-center items-center h-screen">Hello, World!</h1>
+    <h1 className="text-4xl font-bold text-primary flex justify-center items-center h-screen">Hello, World!</h1>
   );
 }
